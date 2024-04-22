4eb87dc203aff7dd5f5c11832fe368599084168f
Eduardo Morbeck <156026527+EduardoMorbeck@users.noreply.github.com>
2024-04-22 09:31:16 -0300

    Update Turma.java

--- Projeto Integrador/src/Turma.java
+++ Projeto Integrador/src/Turma.java
@@ -1,13 +1,10 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Turma {
     private String codigo;
     private String etapaEnsino;
     private int ano;
     private int limiteVagas;
     private int numMatriculados;
-    private ArrayList<Aluno> alunos;
+    private ListaDeAlunos alunos;
     
     public Turma(String codigo, String etapaEnsino, int ano, int limiteVagas, int numMatriculados) {
         this.codigo = codigo;
@@ -15,33 +12,17 @@ public class Turma {
         this.ano = ano;
         this.limiteVagas = limiteVagas;
         this.numMatriculados = 0;
-        this.alunos = new ArrayList<>();
+        this.alunos = new ListaDeAlunos(limiteVagas);
     }
     
-    public void matricularAluno(Aluno aluno) {
+    public void matricularAluno(Aluno aluno) throws ExcecaoDeAlunoJaExistente {
         if (numMatriculados < limiteVagas) {
-            alunos.add(aluno);
+            alunos.incluirNoFim(aluno);
             numMatriculados++;
-            System.out.println("Aluno " + aluno.getNome() + "matriculado com sucesso na turma " + codigo);
+            System.out.println("Aluno " + aluno.toString() + " matriculado com sucesso na turma " + codigo);
         } else {
-            System.out.println("Não foi possível matricular o aluno "+ aluno + "na turma " + codigo + ". Turma lotada.");
+            System.out.println("Não foi possível matricular o aluno "+ aluno + " na turma " + codigo + ", turma lotada.");
         }
-    }
-
-    public static Turma cadastrarTurma(Scanner scanner) {
-        System.out.println("Digite o código da turma:");
-        String codigo = scanner.nextLine();
-        System.out.println("Digite a etapa de ensino (Infantil, Fundamental anos iniciais, Fundamental anos finais, Médio):");
-        String etapaEnsino = scanner.nextLine();
-        System.out.println("Digite o ano da turma:");
-        int ano = scanner.nextInt();
-        System.out.println("Digite o limite de vagas:");
-        int limiteVagas = scanner.nextInt();
-        System.out.println("Digite o número de matriculados");
-        int numMatriculados = scanner.nextInt();
-        
-
-        return new Turma(codigo, etapaEnsino, ano, limiteVagas, numMatriculados);
     }
     
     public String getCodigo() {
@@ -64,7 +45,22 @@ public class Turma {
         return numMatriculados;
     }
     
-    public ArrayList<Aluno> getAlunos() {
-        return alunos;
+    public Iterable<Aluno> getAlunos() {
+        return alunos.getAlunos();
+    }
+
+    @Override
+    public String toString() {
+        return "Turma: " + codigo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Turma turma = (Turma) obj;
+        return codigo.equals(turma.codigo);
     }
 }
